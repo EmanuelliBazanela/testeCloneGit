a8909d3b70465104406a7c7f78200427d11271ae
brunloyola02 <brunloyola02@gmail.com>
2020-02-14 08:25:40 -0300

    fiz o que o prof pediu...

--- teste.c
+++ teste.c
@@ -2,7 +2,12 @@
 #include <stdio.h>
 
 int main(){
+    int idade, resultado;
+    printf("Coloque a sua idade:");
+    scanf("%i", &idade);
 
+    resultado=idade -1;
+    printf("\n Sua idade agora: %i \n",resultado);
 
     return 0;
 }
